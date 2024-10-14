051c71d0f362c4702ac707fc247b69bdc3020ac5
selvaguru-s <selvaguru2017@gmail.com>
2024-10-14 10:18:48 +0100

    21th

--- tasks/celery_app.py
+++ tasks/celery_app.py
@@ -1,8 +1,20 @@
 from celery import Celery
 
-# Initialize Celery with Redis as the broker
-celery = Celery(
-    __name__,
-    broker='redis://localhost:7000/0'
-)
+def make_celery():
+    # Initialize Celery with Redis as the broker
+    celery = Celery(
+        __name__,
+        broker='redis://localhost:7000/0',
+        backend='redis://localhost:7000/0'  # Optional: if you want to store results
+    )
+    
+    celery.conf.update(
+        task_routes={
+            'tasks.tools.c_nmap.perform_scan': {'queue': 'scan'},
+            
+        }
+    )
+    
+    return celery
 
+celery = make_celery()
