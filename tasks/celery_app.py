f20f787f7f9d3b822af67390a33a070e5f8f715f
selvaguru-s <selvaguru2017@gmail.com>
2024-10-14 11:15:27 +0100

    24th

--- tasks/celery_app.py
+++ tasks/celery_app.py
@@ -5,18 +5,10 @@ def make_celery():
     celery = Celery(
         __name__,
         broker='redis://localhost:7000/0',
-        backend='redis://localhost:7000/0'  # Optional: if you want to store results
+       
     )
     
-    celery.conf.update(
-        task_routes={
-            'tasks.tools.c_nmap.perform_scan': {'queue': 'scan'},
-            'tasks.tools.c_whatweb.perform_whatweb':{'queue':'whatweb'},
-            'tasks.tools.c_networktools.perform_network_tool':{'queue':'basic'},
-            'tasks.tools.c_sublist3r.perform_sublist3r':{'queue':'sublist3r'}
-            
-        }
-    )
+
     
     return celery
 
