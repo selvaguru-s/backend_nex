a36680a6988561cf2a2f96040ffa5baf4fef11b6
selvaguru-s <selvaguru2017@gmail.com>
2024-10-14 15:11:04 +0100

    37th

--- tasks/celery_app.py
+++ tasks/celery_app.py
@@ -5,13 +5,19 @@ def make_celery():
     celery = Celery(
         __name__,
         broker='redis://localhost:7000/0',
-        include=['tasks.tools.c_nmap']  # Include the module where your tasks are located
+        include=['tasks.tools.c_nmap'],
+        include=['tasks.tools.c_networktools'],
+        include=['tasks.tools.c_whatweb'] ,
+        include=['tasks.tools.c_sublist3r']  # Include the module where your tasks are located
     )
 
     # You can add more configuration here if needed
     celery.conf.update(
         task_routes={
-            'tasks.tools.c_nmap.perform_scan': {'queue': 'scan'}
+            'tasks.tools.c_nmap.perform_scan': {'queue': 'scan'},
+            'tasks.tools.c_networktools.perform_network_tool': {'queue': 'basic'},
+            'tasks.tools.c_sublist3r.perform_sublist3r': {'queue': 'sublist3r'},
+            'tasks.tools.c_whatweb.perform_whatweb': {'queue': 'whatweb'}
         }
     )
 
