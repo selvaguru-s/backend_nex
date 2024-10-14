0f1b369ded27ba9dc3c4cb514a673094f221ef2d
selvaguru-s <selvaguru2017@gmail.com>
2024-10-14 12:34:50 +0100

    36th

--- tasks/celery_app.py
+++ tasks/celery_app.py
@@ -5,12 +5,16 @@ def make_celery():
     celery = Celery(
         __name__,
         broker='redis://localhost:7000/0',
-         
-       
+        include=['tasks.tools.c_nmap']  # Include the module where your tasks are located
     )
-    
 
-    
+    # You can add more configuration here if needed
+    celery.conf.update(
+        task_routes={
+            'tasks.tools.c_nmap.perform_scan': {'queue': 'scan'}
+        }
+    )
+
     return celery
 
 celery = make_celery()
